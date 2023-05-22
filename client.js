0ab8f496341d0fcdede75c9bc2c937f00c97fe78
workuseifu1 <workuseifu1@gmail.com>
2023-05-22 04:46:35 -0600

    add handler for Ctrl +c user input

--- client.js
+++ client.js
@@ -11,18 +11,25 @@ const connect = function () {
 
   // handling message from server
   conn.on("data", (data) => {
-    console.log(data)
+    console.log(data);    
   });
   //print a message to the screen and send name of the player to server
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
+  conn.on("connect", () => {    
     conn.write("Name: W");
-    // send the string moves as data to the server
-    setTimeout(() => { conn.write("Move: up"); }, 50)
-    setTimeout(() => { conn.write("Move: left"); }, 150)
-    setTimeout(() => { conn.write("Move: down"); }, 100)
-    setTimeout(() => { conn.write("Move: right"); }, 200)
   });
+  conn.on("connect", () => {
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // setTimeout(() => { conn.write("Move: up"); }, 50)
+    // setTimeout(() => { conn.write("Move: left"); }, 150)
+    // setTimeout(() => { conn.write("Move: down"); }, 100)
+    // setTimeout(() => { conn.write("Move: right"); }, 200)
+  });
+    // send the string moves as data to the server
+    
+  
   return conn;
 }
 
--- play.js
+++ play.js
@@ -4,5 +4,20 @@ const net = require("net");
 
 const {connect} = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();  
+  stdin.on("data", handleUserInput);  
+  return stdin;
+};
+// when Ctrl + C key pressed on a keyboard it will exit
+const handleUserInput = function (key) {    
+ if(key === '\u0003') {  
+   process.exit();
+ } 
+};
 connect();
+setupInput();
 
